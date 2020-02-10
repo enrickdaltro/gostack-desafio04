0cf0447d770b852845cdca3b74d658a146f22770
Enrick <enrickdaltro@gmail.com>
2020-02-10 09:55:02 -0300

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+    return (
+        <div id="header">
+            <div className="content">
+                <div className="left">
+                    <h2>facebook</h2>
+                </div>
+                <div className="right">
+                    <h5>Meu Perfil</h5>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
